af0f66c3a7cc121af96d6f8912b46322df1ccf76
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2016-11-03 22:01:59 +0300

    Add support for webpack2

--- combineLoaders.js
+++ combineLoaders.js
@@ -2,10 +2,22 @@ var qs = require('qs');
 
 function combineLoaders(loaders) {
   return loaders.map(function(loaderEntry) {
-    var query = qs.stringify(loaderEntry.query, { arrayFormat: 'brackets', encode: false });
+    // Careful, won't work if a string is created with `new String('my-loader')`
+    // as the type will be Object instead
+    if (typeof loaderEntry === 'string') {
+      return loaderEntry;
+    }
+
+    var query = qs.stringify(
+      loaderEntry.options || loaderEntry.query, {
+        arrayFormat: 'brackets',
+        encode: false,
+      });
+
     if (query) {
       query = '?' + query;
     }
+
     return loaderEntry.loader + query;
   }).join('!');
 }
