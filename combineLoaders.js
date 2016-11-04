2c726a4c282566c356d2371fce6f0dc5973aa768
James Friend <james@kitefire.com>
2016-11-03 17:20:31 -0700

    add eslint and clean things up

--- combineLoaders.js
+++ combineLoaders.js
@@ -1,25 +1,23 @@
 var qs = require('qs');
 
 function combineLoaders(loaders) {
-  return loaders.map(function(loaderEntry) {
-    // Careful, won't work if a string is created with `new String('my-loader')`
-    // as the type will be Object instead
-    if (typeof loaderEntry === 'string') {
-      return loaderEntry;
-    }
+    return loaders.map(function(loaderEntry) {
+        if (typeof loaderEntry === 'string') {
+            return loaderEntry;
+        }
 
-    var query = qs.stringify(
+        var query = qs.stringify(
       loaderEntry.options || loaderEntry.query, {
-        arrayFormat: 'brackets',
-        encode: false,
+          arrayFormat: 'brackets',
+          encode: false,
       });
 
-    if (query) {
-      query = '?' + query;
-    }
+        if (query) {
+            query = '?' + query;
+        }
 
-    return loaderEntry.loader + query;
-  }).join('!');
+        return loaderEntry.loader + query;
+    }).join('!');
 }
 
 module.exports = combineLoaders;
